6cbda848e99bfa61ec04268f52f0d661d45e875a
oya riku <riku03290@gmail.com>
2021-01-23 23:34:56 +0900

    Update:README.md

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # LeetCode
+
+2020年12月ごろから継続しているWrite code every dayの習慣としてLeetCodeの問題を解答を記録してる
+
---
+++ typescript/14.longest-common-prefix.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=14 lang=typescript
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+function longestCommonPrefix(strs: string[]): string {
+
+};
+// @lc code=end
+
