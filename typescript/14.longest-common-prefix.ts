7881dc2b0409b1af7444e4bb72174c518c319b3a
oya riku <riku03290@gmail.com>
2021-01-24 23:54:13 +0900

    Add:longest common  prefix

--- typescript/14.longest-common-prefix.ts
+++ typescript/14.longest-common-prefix.ts
@@ -5,8 +5,27 @@
  */
 
 // @lc code=start
-function longestCommonPrefix(strs: string[]): string {
+function longestCommonPrefix(strs: string[]): any {
+  let common = "";
 
-};
+  if (strs.length === 0) {
+    return common;
+  }
+
+  let indexWord = strs[0];
+  let indexLength = 0;
+  for (let letter of indexWord) {
+    for (let i = 1; i < strs.length; i++) {
+      let currentWord = strs[i];
+      let currentLetter = currentWord.charAt(indexLength);
+
+      if (letter !== currentLetter || indexLength > currentWord.length) {
+        return common;
+      }
+    }
+    indexLength++;
+    common += letter;
+  }
+  return common;
+}
 // @lc code=end
-
