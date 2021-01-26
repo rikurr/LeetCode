798e6523a66b4a38c29617377b17b246a83f94e2
oya riku <riku03290@gmail.com>
2021-01-26 22:37:47 +0900

    Add:valid-parentheses

---
+++ typescript/20.valid-parentheses.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode id=20 lang=typescript
+ *
+ * [20] Valid Parentheses
+ */
+
+// @lc code=start
+function isValid(s: string): any {
+  console.log(s.trim()[s.length / 2]);
+  for (let i = 0; i < s.length; i++) {}
+}
+// @lc code=end
