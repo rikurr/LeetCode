d394e4d7820dbdcbdb0d955d2b5830928f037adf
oya riku <riku03290@gmail.com>
2021-01-28 11:10:35 +0900

    Updat:再帰関数処理に変更

--- typescript/9.palindrome-number.ts
+++ typescript/9.palindrome-number.ts
@@ -5,24 +5,20 @@
  */
 
 // @lc code=start
+function palindrome(s: string) {
+  if (s.length <= 1) {
+    return true;
+  }
+  if (s[0] !== s[s.length - 1]) {
+    return false;
+  }
+  const x = s.slice(1).slice(0, -1);
+  return palindrome(x);
+}
 function isPalindrome(x: number): boolean {
   if (Math.sign(x) === -1) {
     return false;
   }
-  let xString = x.toString();
-  let left = 0;
-  let right = xString.length - 1;
-  let result = true;
-
-  while (left <= right) {
-    if (xString[left] === xString[right]) {
-      left++;
-      right--;
-    } else {
-      result = false;
-      break;
-    }
-  }
-  return result;
+  return palindrome(x.toString());
 }
 // @lc code=end
